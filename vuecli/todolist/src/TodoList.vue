af903023456fde24fb1481f70a4ec31a11f65131
shanxiansen310 <43680689+shanxiansen310@users.noreply.github.com>
2021-03-24 18:20:27 +0800

    initial

---
+++ vuecli/todolist/src/TodoList.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div>
+      <input v-model="inputValue">
+      <button @click="handleSubmit">Submit</button>
+    </div>
+    <ul>
+      <todo-item
+        v-for="(item,index) of list"
+        :key="index"
+        v-bind:content="item"
+        v-bind:index="index"
+        @delete="handleDelete"
+      ></todo-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem'
+
+export default {
+  /* 这两种声明方法都是可以的!!!  */
+  components: {
+    TodoItem, // template中命名为<TodoItem>
+    todoItem: TodoItem, // template中命名为<todo-item>
+    'todo-item': TodoItem // template中命名为<todo-item>
+  },
+  /* 在这里必须要以函数的形式返回 */
+  data () {
+    return {
+      inputValue: '',
+      list: []
+    }
+  },
+  methods: {
+    handleSubmit () {
+      this.list.push(this.inputValue)
+      this.inputValue = ''
+    },
+    handleDelete (index) {
+      this.list.splice(index, 1) // index是由子组件传递过来的
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vuecli/todolist/src/components/TodoItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <li @click="handleDelete">{{ content }}</li>
+</template>
+
+<script>
+export default {
+  props: ['content', 'index'], // 父组件借此向子组件传值
+  methods: {
+    handleDelete: function () {
+      this.$emit('delete', this.index) // 抛出一个事件,让父组件定义
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
